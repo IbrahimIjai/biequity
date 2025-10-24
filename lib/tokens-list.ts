c1c0a552284d22203eb3fe0e197e832f3918636c
ibrahimsunday <ibrahimsundayijai@gmail.com>
2025-10-24 17:28:24 +0100

    feat: add missing addresses for stablecoins and stocks in tokens list

--- lib/tokens-list.ts
+++ lib/tokens-list.ts
@@ -4,6 +4,7 @@ export const STABLECOINS = [
 		name: "USD Coin",
 		icon: "/tokens/usdc.png",
 		decimals: 6,
+		address: "0x036CbD53842c5426634e7929541eC2318f3dCF7e",
 	},
 ];
 
@@ -13,17 +14,22 @@ export const STOCKS = [
 		name: "Apple Stock",
 		icon: "/tokens/AAPL.png",
 		decimals: 18,
+		address: "0x0000000000000000000000000000000000000000AAPL",
 	},
 	{
 		symbol: "TSLA",
 		name: "Tesla Stock",
 		icon: "/tokens/TSLA.png",
 		decimals: 18,
+
+		address: "0x0000000000000000000000000000000000000000TSLA",
 	},
 	{
 		symbol: "MSFT",
 		name: "Microsoft Stock",
 		icon: "/tokens/MSFT.png",
 		decimals: 18,
+
+		address: "0x0000000000000000000000000000000000000000AAPL",
 	},
 ];
