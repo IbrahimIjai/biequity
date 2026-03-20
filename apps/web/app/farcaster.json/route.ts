289ab6fb982b3ea3d45badaf4168fe333f34d906
IbrahimIjai <sunday.m1701072@st.futminna.edu.ng>
2026-03-20 02:10:22 +0100

    Migration to turbo repo

---
+++ apps/web/app/farcaster.json/route.ts
@@ -0,0 +1,6 @@
+import { withValidManifest } from "../../providers/minikit-manifest";
+import { minikitConfig } from "../../minikit.config";
+
+export async function GET() {
+	return Response.json(withValidManifest(minikitConfig));
+}
--- old/app/.well-known/farcaster.json/route.ts
+++
@@ -1,6 +0,0 @@
-import { withValidManifest } from "../../../providers/minikit-manifest";
-import { minikitConfig } from "../../../minikit.config";
-
-export async function GET() {
-	return Response.json(withValidManifest(minikitConfig));
-}
